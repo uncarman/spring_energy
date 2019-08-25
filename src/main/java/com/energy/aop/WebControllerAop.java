efa646115437304c311f9871abbefd70bcd994dc
wang <sam.wan@sunallies.com>
2019-08-26 07:27:18 +0800

    添加日志

---
+++ src/main/java/com/energy/aop/WebControllerAop.java
@@ -0,0 +1,72 @@
+package com.energy.aop;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.energy.config.LogConfig;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import javax.servlet.http.HttpServletRequest;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Administrator on 2019/8/26.
+ */
+
+@Aspect
+@Component
+public class WebControllerAop {
+
+    private static final Logger logger = LoggerFactory.getLogger(WebControllerAop.class);
+
+    @Pointcut("execution(public * com.energy.controller.*.*(..))")
+    public void webLog(){
+    }
+
+    @Before("webLog()")
+    public void doBefore(JoinPoint joinPoint){
+        // 接收到请求，记录请求内容
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest req = attributes.getRequest();
+        // 记录下请求内容
+        logger.warn("request: " + req.getRemoteAddr() + " " + req.getRequestURL().toString());
+
+        //获取目标方法的参数信息
+        Signature signature = joinPoint.getSignature();
+        MethodSignature methodSignature = (MethodSignature) signature;
+        String[] strings = methodSignature.getParameterNames();
+        Map<String, String> args = new HashMap<>();
+        if(null != strings) {
+            for(int i = 0; i < strings.length; i++) {
+                args.put(strings[i], String.valueOf(joinPoint.getArgs()[i]));
+            }
+        }
+        String argStr = JSON.toJSONString(args);
+        logger.warn("params: " + argStr);
+    }
+
+    /**
+     * 处理完请求返回内容
+     * @param ret
+     * @throws Throwable
+     */
+    @AfterReturning(returning = "ret", pointcut = "webLog()")
+    public void doAfterReturning(Object ret) throws Throwable {
+        // 处理完请求，返回内容
+        logger.warn("response: " + JSON.toJSONString(ret));
+    }
+
+}
---
+++ src/main/java/com/energy/config/LogConfig.java
@@ -0,0 +1,16 @@
+package com.energy.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Administrator on 2019/8/26.
+ */
+
+
+@Configuration
+public class LogConfig {
+    private static final Logger LOG = LoggerFactory.getLogger(LogConfig.class);
+}
